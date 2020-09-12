c6ed193b1d5a37966464ad818c4ab0f1afc672db
Jason Hinton <jason_hinton@att.net>
2020-09-12 00:14:11 -0400

    comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,30 +24,36 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [x.name for x in humans if x.name[0] == 'D']
+
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [x.name for x in humans if x.name[-1] == 'e']
+
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+y = ['C', 'D', 'E', 'F', 'G']
+c = [x.name for x in humans if x.name[0] in y]
+
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [x.age + 10 for x in humans]
+
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{x.name}-{x.age}" for x in humans]
+
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
